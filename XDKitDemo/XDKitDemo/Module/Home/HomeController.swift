6581da18751d941bab6467d122d0aee774114f8d
Dyl <329720990@qq.com>
2022-06-13 18:12:14 +0800

    fix

--- XDKitDemo/XDKitDemo/Module/Home/HomeController.swift
+++ XDKitDemo/XDKitDemo/Module/Home/HomeController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxDataSources
+import SnapKit
 
 class HomeController: UIViewController {
 
@@ -14,7 +15,15 @@ class HomeController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
-     
+        let label = XDLabel()
+        label.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 30, right: 40)
+        label.backgroundColor = UIColor.red
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.left.top.equalTo(100)
+        }
+        
+        label.text = "asdaidhaudhasydfas"
         
     }
     
